16e4ee6724625f0ea1f75607828f7266748b9d4c
gagi <gagi.shmagi@gmail.com>
2021-02-09 09:52:03 +0200

    Begining of timer project

---
+++ src/Components/Timer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap';
+import DateTimePicker from 'react-datetime-picker'
+
+export default class Timer extends Component {
+    render() {
+        return (
+            <div>
+                Timer
+            </div>
+        )
+    }
+}
